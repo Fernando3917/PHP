e9fc0b477ce3a54adc4fe75c5714231c4e4505be
Fernando Alves <fa8361272@gmail.com>
2022-04-18 15:19:48 -0300

    Update oparitimeticas.php

--- ex/oparitimeticas.php
+++ ex/oparitimeticas.php
@@ -8,9 +8,10 @@
 
 
 <?php
-$n1 = 15;
+		
+$n1 = 15; // $_GET["a"];  -- vc pode usar o get para procurar variaveis 
 $n2 = 10;
-
+$x = 20;
 
 
 echo "adição: ";
@@ -28,6 +29,43 @@ echo $r;
 echo "<br>modulo(resto da divisão): ";
 $r = $n1%$n2;
 echo $r;
+
+//---------------------------//-------------//Funções prontas no php-----
+
+//valor absoluto: 
+$a = -3;
+echo "<br>O valor absoluto de $a e". abs($a);
+
+//Potenciação: 
+$a = 4; 
+$b = 3;
+echo "<br>A potenciação de $a e ". pow($a, $b);
+
+//raiz quadrada: 
+$a = 3;
+$b = 4;
+echo "<br> Raiz quadrada de $a e ".sqrt($a);
+
+//arredondamento de valores reais:
+$a = 3;
+$b = 4;
+echo "<br> O valor redondo de $a e ".round($a); //ceil --> arredonda sempre pra cima , floor ___> arredonda sempre para baixo.
+
+
+//raiz quadrada: 
+$a = 3;
+$b = 4;
+echo "<br> Raiz quadrada de $a e ".sqrt($a);
+
+//inteiro de um numero real
+$a = 3;
+$b = 4;
+echo "<br> Pega a parte inteira de $a e ".intval($a);
+
+//Formatar um numero em algum valor
+$a = 4;
+$b = 4;
+echo "<br> formatar com 4 casas cecimais $a e ".number_format($a,4);
 
 
 
