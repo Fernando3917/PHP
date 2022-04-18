867056881e8e557a6bacb9d30aac41541c10ec33
Fernando Alves <fa8361272@gmail.com>
2022-04-18 13:22:17 -0300

    Create oparitimeticas.php

---
+++ ex/oparitimeticas.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html>
+<body>
+<h1>Aula de operadores aritimeticos</h1>
+<h3>n1 = 15  n2 = 10</h3>
+
+
+
+
+<?php
+$n1 = 15;
+$n2 = 10;
+
+
+
+echo "adição: ";
+$resultado = $n1+$n2;
+echo $resultado;
+
+echo "<br>subtração: ";
+$r = $n1-$n2;
+echo $r;
+
+echo "<br>divisão: ";
+$r = $n1/$n2;
+echo $r;
+
+echo "<br>modulo(resto da divisão): ";
+$r = $n1%$n2;
+echo $r;
+
+
+
+?>
+</body>
+</html>
